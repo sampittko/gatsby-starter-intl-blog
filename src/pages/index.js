ccd72dde3a8b36f733e872e3174e8016fbdb8965
Samuel Pitoňák <sampittko@gmail.com>
2020-05-26 21:53:56 +0200

    Set overflow hidden on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const IndexPage = ({ pageContext, data, intl }) => {
         lang={pageContext.intl.language}
         description={intl.formatMessage({ id: "page.index.description" })}
       />
-      <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center h-screen overflow-hidden">
         <div className="flex flex-col justify-center items-center h-content h-48 md:h-20 lg:h-26 w-screen transform -skew-y-11 bg-brown">
           <Profile className="absolute -top-26 lg:-top-32 md:inset-x-0 md:mx-auto md:w-content-compact lg:w-content" />
           <div

af182cb646fc5b65b264c1a62749ee677db9a388
Samuel Pitoňák <sampittko@gmail.com>
2020-07-19 15:35:00 +0200

    Change opacity of footer, add missing graphql import

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -3,7 +3,7 @@ import LanguagePicker from "./LanguagePicker";
 import Note from "./note/Note";
 
 const Footer = () => (
-  <footer className="fixed bottom-0 px-10 py-1 sm:px-5 sm:py-5 flex flex-row justify-between w-screen w-full max-w-screen-md bg-white shadow-2xl sm:shadow-none bg-opacity-25">
+  <footer className="fixed bottom-0 px-10 py-1 sm:px-5 sm:py-5 flex flex-row justify-between w-screen w-full max-w-screen-md bg-white shadow-2xl sm:shadow-none bg-opacity-50">
     <LanguagePicker />
     <Note />
   </footer>
--- src/components/layout/footer/note/Developer.js
+++ src/components/layout/footer/note/Developer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useStaticQuery } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 import Link from '../../../Link';
 import { injectIntl } from 'gatsby-plugin-intl';
 
